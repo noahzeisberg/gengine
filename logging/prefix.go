e03c0c9bb3847365b01c9551bc6f85e686105519
NoahOnFyre <noahonfyre@gmail.com>
2023-09-22 21:53:06 +0200

    Update prefix.go

--- logging/prefix.go
+++ logging/prefix.go
@@ -5,7 +5,7 @@ import "github.com/NoahOnFyre/gengine/color"
 func Prefix(level int) string {
 	switch level {
 	case 0:
-		return color.Black + color.GreenBg + " INFO " + color.Reset + color.Reset + " "
+		return color.Black + mainColor + " INFO " + color.Reset + color.Reset + " "
 	case 1:
 		return color.Black + color.YellowBg + " WARN " + color.Reset + color.Yellow + " "
 	case 2:
